1384227e8e5df208e2df8f0a9d3c92a505422a70
ashwinkashok <ashwinkashok@gmail.com>
2017-09-16 14:34:01 +0530

    Branch commit Synchronous http

--- app/src/main/java/com/example/ashwi/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/ashwi/weatherapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.ashwi.weatherapp;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String apiKEY="ce1ee29102b40e836ba66130b2053d01";
-        double lattitude=37.8267;
+        double latitude=37.8267;
         double longitude=-122.4233;
 
-        String forecastURL="https://api.darksky.net/forecast/" + apiKEY + "/"+lattitude+","+longitude;
+        String forecastURL="https://api.darksky.net/forecast/" + apiKEY + "/"+latitude+","+longitude;
 
         OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        final String TAG = MainActivity.class.getSimpleName();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful()){
+                Log.v( TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e( TAG,"Exception caught",e);
+        }
+
+
     }
 }
